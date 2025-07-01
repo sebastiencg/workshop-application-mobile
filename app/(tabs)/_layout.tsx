d2613905dcead234be5c0d29b8b289fa38361a02
jeremy <nashoba.jeremy@gmail.com>
2025-07-01 13:26:43 +0200

    Map and modals for shows and stands

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -45,7 +45,7 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="map" color={color} />,
         }}
       />
       <Tabs.Screen
